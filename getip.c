2e4cd14e19248bc401565241722c37ca49711a88
Khamkou Uladzislau <hamkov@vivaldi.net>
2020-12-13 00:56:39 +0300

    add socket closing

--- getip.c
+++ getip.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <linux/wireless.h>
@@ -13,7 +14,8 @@ char *ifaddrs2str(struct ifaddrs *ifa) {
     strcpy(req.ifr_ifrn.ifrn_name, ifa->ifa_name);
     req.u.essid.pointer = essid;
     req.u.essid.length = 32;
-    ioctl(socket(AF_INET, SOCK_DGRAM, 0), SIOCGIWESSID, &req);
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    ioctl(sock, SIOCGIWESSID, &req); close(sock);
     getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr), ip, 16, NULL, 0, 1);
     char *res = malloc(strlen(ifa->ifa_name) + strlen(ip) + strlen(essid) + 4);
     sprintf(res, "%s:%s<%s>", ifa->ifa_name, essid, ip);
@@ -29,4 +31,5 @@ int main() {
             || strcmp(ifa->ifa_name, "lo") == 0) continue;
         printf("%s\n", ifaddrs2str(ifa));
     }
+    free(ifas);
 }
